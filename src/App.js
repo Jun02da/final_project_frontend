9b7f9aee981fd26d321a235f1c78a6c9a149e7c3
unknown <yjhan713@naver.com>
2023-03-09 14:55:32 +0900

    han

--- src/App.js
+++ src/App.js
@@ -1,23 +1,136 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef } from "react";
+import Sidebar from "./Sidebar.js";
+import image_import from "./img/img01.png"; // eslint-disable-line no-unused-vars
+import "./css/profile.css";
+import "./css/header.css";
+import "./css/list.css";
+import "./css/Sidebar.css";
 
-function App() {
+import {
+  AiOutlineUnorderedList,
+  AiOutlineUpCircle,
+  AiOutlinePlusCircle,
+} from "react-icons/ai";
+
+function App(props) {
+  const [imageUrl, setImageUrl] = useState(null);
+  const imgRef = useRef();
+
+  const onChangeImage = () => {
+    const reader = new FileReader();
+    const file = imgRef.current.files[0];
+    console.log(file);
+
+    reader.readAsDataURL(file);
+    reader.onloadend = () => {
+      setImageUrl(reader.result);
+      console.log("이미지주소", reader.result);
+    };
+  };
+
+  const onClickFileBtn = (e) => {
+    imgRef.current.click();
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="body">
+      <div className="header_main">
+        <header className="header">
+          <h2>POPO</h2>
+          <nav>
+            <div>
+              <div className="header_li">
+                <input type="text" placeholder="검색"></input>
+              </div>
+              <div>
+                <Sidebar />
+              </div>
+            </div>
+          </nav>
+        </header>
+      </div>
+
+      <div className="profile_main">
+        <section className="section">
+          <img
+            className="image"
+            src={
+              imageUrl
+                ? imageUrl
+                : "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_1280.png"
+            }
+            width={150}
+            height={150}
+            style={{ borderRadius: "50%" }}
+            onClick={() => {
+              onClickFileBtn();
+            }}
+          />
+          <input
+            type="file"
+            ref={imgRef}
+            onChange={onChangeImage}
+            style={{ display: "none" }}
+          />
+          <div className="profile">
+            <div className="item">
+              <div>yjhan713</div>
+              <div>Hello</div>
+            </div>
+            <div className="item">
+              <div>
+                <button className="profile_button">프로필 편집</button>
+              </div>
+            </div>
+          </div>
+          <div className="summaryContainer">
+            <div className="item">
+              <div>게시물</div>
+              <div className="number">5</div>
+            </div>
+            <div className="item">
+              <div>팔로워</div>
+              <div className="number">1500</div>
+            </div>
+            <div className="item">
+              <div>방문자</div>
+              <div className="number">600</div>
+            </div>
+          </div>
+        </section>
+      </div>
+
+      <div className="list_main">
+        <article className="article">
+          <div>
+            <button className="button_active">
+              <p className="list">
+                <AiOutlineUnorderedList />
+                게시물
+              </p>
+            </button>
+          </div>
+          <div>
+            <button className="button_active">
+              <p className="save">
+                <AiOutlineUpCircle />
+                저장됨
+              </p>
+            </button>
+          </div>
+          <div>
+            <button className="button_active">
+              <p className="upload">
+                <AiOutlinePlusCircle />
+                업로드
+              </p>
+            </button>
+          </div>
+        </article>
+        <div>
+          <img src={image_import} alt="img01" />
+        </div>
+      </div>
     </div>
   );
 }
