53edb209439dbf26525a36eac27457251a379abb
Topping77 <stillalive77@naver.com>
2023-03-16 09:10:20 +0900

    이미지카드 수정

--- src/components/ImgCard.js
+++ src/components/ImgCard.js
@@ -2,13 +2,21 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-import ImageListItemBar from '@mui/material/ImageListItemBar';
 import {Link} from 'react-router-dom';
 import test from './ImgCard.json' //ImgCard.json <<<< 테스트용 데이터
-import IconButton from '@mui/material/IconButton';
-import InfoIcon from '@mui/icons-material/Info';
+import { MDBRipple } from 'mdb-react-ui-kit';
 
    
+
+{/* <MDBRipple className='hover-zoom' rippleTag='div' rippleColor='light'>
+
+<div className='mask hover-overlay' style={{ backgroundColor: 'rgba(0, 0, 0, 0.4)' }}>
+            <div className='d-flex justify-content-center align-items-center h-100'>
+              <p className='text-white mb-0'>가나다</p>
+            </div>
+</div>
+
+</MDBRipple> */}
 
 
 
@@ -28,11 +36,21 @@ export default function MasonryImageList() {
       {test.itemData.map((item) => (
         <ImageListItem key={item.img} className="bg-image hover-zoom">
           <Link to="detail">
+
+            
             <img
               src={`${item.img}?w=400&fit=crop&auto=format`}
               srcSet={`${item.img}?w=248&fit=crop&auto=format&dpr=2 2x`}
               alt={item.title}
+              onMouseOver = {()=>{<div className='mask hover-overlay' style={{ backgroundColor: 'rgba(0, 0, 0, 0.4)' }}>
+              <div className='d-flex justify-content-center align-items-center h-100'>
+                <p className='text-white mb-0'>가나다</p>
+              </div>
+  </div>}}
             />
+
+            
+
           </Link>
         </ImageListItem>
       ))}
