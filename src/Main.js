c37741923ec5a0b0c6101fc41d6795919a569f3d
thecommn <yonjunkim20@gmail.com>
2023-03-16 13:04:36 +0900

    detail 페이지 수정

--- src/Main.js
+++ src/Main.js
@@ -1,19 +1,38 @@
-import React from "react";
+import { React, useState, useEffect } from "react";
 import "./css/Main.css";
-import FirstFeed from "./FirstFeed";
+import Feed from "./components/Feed";
 
-class Main extends React.Component {
-  render() {
-    return (
-      <>
-        <main>
-          <div className="wrapMain">
-            <FirstFeed />
-          </div>
-        </main>
-      </>
-    );
-  }
+export default function Main() {
+  const [commentData, setCommentData] = useState([]);
+  const [feedData, setFeedData] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/data/commentData.json", {
+      method: "GET",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setCommentData(data);
+      });
+  }, []);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/data/feedData.json", { method: "GET" })
+      .then((res) => res.json())
+      .then((data) => {
+        setFeedData(data);
+      });
+  }, []);
+
+  return (
+    <div className="main">
+      <div className="body">
+        <Feed
+          feedData={feedData}
+          commentData={commentData}
+          setCommentData={setCommentData}
+        />
+      </div>
+    </div>
+  );
 }
-
-export default Main;
--- src/components/Comment.js
+++ src/components/Comment.js
@@ -1,73 +1,24 @@
-import { useState } from "react";
-import CommentList from "./CommentList";
-import classnames from "classnames";
-import moment from "moment";
-// 안써도 자동으로 한국 시간을 불러온다. 명확하게 하기 위해 import
-import "moment/locale/ko";
-function Comment() {
-  const [userName] = useState("user");
-  const [comment, setComment] = useState("");
-  const [feedComments, setFeedComments] = useState([]);
-  const [isValid, setIsValid] = useState(false);
-  const [date, setDate] = useState([]);
-  // 댓글 구현 함수()
-  const post = (e) => {
-    const copyFeedCommnets = [...feedComments];
-    copyFeedCommnets.push(comment);
-    setFeedComments(copyFeedCommnets);
-    // 게시 버튼을 누르면 초기로 돌아감
-    setDate(moment().format("YYYY-MM-DD HH:mm:ss"));
-    setComment("");
-    setIsValid(false);
-  };
+import { React } from "react";
+import CommentHeart from "./CommentHeart";
 
+const Comment = ({ feedNum, commentData }) => {
   return (
-    <div className="container">
-      <form className="feedComment">
-        {/* 댓글 입력하는 곳 */}
-        <input
-          type="text"
-          className="commentText"
-          placeholder="댓글 달기"
-          onChange={(e) => {
-            setComment(e.target.value);
-          }}
-          onKeyUp={(e) => {
-            e.target.value.length > 0 ? setIsValid(true) : setIsValid(false);
-          }}
-          value={comment}
-        />
-        <button
-          type="button"
-          className={classnames(
-            "commentButton",
-            // 댓글 입력 시 글자가 있다면 활성화, 없다면 비활성화
-            comment.length > 0 ? "submitCommentActive" : "submitCommentInactive"
-          )}
-          onClick={post}
-          // 댓글 등록 이후 버튼 비활성화
-          disabled={isValid ? false : true}
-        >
-          comment
-        </button>
-      </form>
-      {/* 모든 댓글 출력 */}
-      <ul className="commentListContainer">
-        {feedComments.map((commentArr, i) => {
-          return (
-            <li>
-              <CommentList
-                userName={userName}
-                userComment={commentArr}
-                date={date}
-                key={i}
-              />
-            </li>
-          );
-        })}
-      </ul>
+    <div className="commentSection">
+      {commentData[feedNum].map((el) => {
+        return (
+          <div key={el.id}>
+            <span className="id">
+              <strong>{el.name}</strong>
+            </span>
+            {el.comment}
+            <div className="commentImage">
+              <CommentHeart />
+            </div>
+          </div>
+        );
+      })}
     </div>
   );
-}
+};
 
 export default Comment;
