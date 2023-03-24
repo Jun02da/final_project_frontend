f6aa9dd81512e34f762a30f0673d6193d854d477
5junoyung <dyrtiq12@naver.com>
2023-03-24 10:16:12 +0900

    로그인업데이트

--- src/login.js
+++ src/login.js
@@ -1,44 +1,110 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Modal from "react-modal";
 import "./css/login.css";
 import "./css/MainHeader.css";
+import axios from "axios";
 
 Modal.setAppElement("#root");
 
 export default function Login() {
-  const [showModal1, setShowModal1] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
 
-  const handleOpenModal1 = () => {
-    setShowModal1(true);
+  const handleOpenModal = () => {
+    setIsModalOpen(true);
   };
-  const handleCloseModal1 = () => {
-    setShowModal1(false);
+
+  const handleCloseModal = () => {
+    setIsModalOpen(false);
+  };
+
+  const handleEmailChange = (event) => {
+    setEmail(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  useEffect(() => {
+    axios.defaults.headers.common[
+      "Authorization"
+    ] = `Bearer ${localStorage.getItem("token")}`;
+    axios
+      .get("http://192.168.0.209:8090/user/me")
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  const handleLogin = () => {
+    axios
+      .post(
+        "http://192.168.0.209:8090/login",
+        { email, password },
+        { withCredentials: true, crossDomain: true, credentials: "include" }
+      )
+      .then((response) => {
+        const token = response.data;
+        alert("로그인 성공.");
+        if (token) {
+          localStorage.setItem("token", token);
+          axios.defaults.headers.common["Authorization"] = `Bearer ${token}`;
+          alert("토큰 받았습니다.");
+
+          window.location.href = "/";
+        } else {
+          delete axios.defaults.headers.common["Authorization"];
+          alert("토큰 받기 실패");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        alert("로그인에 실패했습니다.");
+      });
   };
 
   return (
     <div>
-      <button onClick={handleOpenModal1} className="NavMenuTitle">
+      <button onClick={handleOpenModal} className="NavMenuTitle">
         로그인
       </button>
       <Modal
-        isOpen={showModal1}
-        onRequestClose={handleCloseModal1}
+        isOpen={isModalOpen}
+        onRequestClose={handleCloseModal}
         className="login_modal"
-        style={{ overlay: { zIndex: 1000 }, content: { zIndex: 1000 } }}
       >
         <h2 className="login">로그인</h2>
+        {errorMessage && <div className="login_error">{errorMessage}</div>}
         <div className="login_form">
           <label htmlFor="email">이메일</label>
-          <input type="email" placeholder="이메일 입력" />
+          <input
+            type="email"
+            placeholder="이메일 입력"
+            value={email}
+            onChange={handleEmailChange}
+          />
         </div>
         <div className="login_form">
           <label htmlFor="password">비밀번호</label>
-          <input type="password" placeholder="비밀번호 입력" />
+          <input
+            type="password"
+            placeholder="비밀번호 입력"
+            value={password}
+            onChange={handlePasswordChange}
+          />
         </div>
         <br />
-        <button className="login_button">로그인</button>
+        <button className="login_button" onClick={handleLogin}>
+          로그인
+        </button>
         <br />
-        <button onClick={handleCloseModal1} className="login_button">
+        <button onClick={handleCloseModal} className="login_button">
           닫기
         </button>
         <br />
@@ -50,12 +116,6 @@ export default function Login() {
         </button>
         <br />
       </Modal>
-      <button
-        onClick={() => (window.location.href = "/MemberShip")}
-        className="NavMenuTitle"
-      >
-        회원가입
-      </button>
     </div>
   );
 }
--- src/membership.js
+++ src/membership.js
@@ -1,42 +1,170 @@
-import React from "react";
+import React, { useState } from "react";
 import "./css/membership.css";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import Login from "./login";
 
-function MemberShip() {
+export default function MemberShip() {
+  const [Memberemail, setMemberEmail] = useState("");
+  const [Memberpassword, setMemberPassword] = useState("");
+  const [Memberpassword1, setMemberPassword1] = useState("");
+  const [Username, setUserName] = useState("");
+  const [Userphonenumber, setUserPhoneNumber] = useState("");
+  const movePage = useNavigate();
+
+  function goHome() {
+    movePage("/");
+  }
+  function goMypage() {
+    movePage("/mypage");
+  }
+  function goBoard() {
+    movePage("/Board");
+  }
+  function goAdmin() {
+    movePage("/Admin");
+  }
+
+  const onMemberemail = (e) => {
+    setMemberEmail(e.target.value);
+  };
+
+  const onMemberpassword = (e) => {
+    setMemberPassword(e.target.value);
+  };
+
+  const onMemberpassword1 = (e) => {
+    setMemberPassword1(e.target.value);
+  };
+
+  const onUsername = (e) => {
+    setUserName(e.target.value);
+  };
+
+  const onUserphonenumber = (e) => {
+    setUserPhoneNumber(e.target.value);
+  };
+
+  const passwordValid = () => {
+    if (Memberpassword !== Memberpassword1) {
+      alert("비밀번호가 일치하지 않습니다.");
+      return false;
+    }
+    return true;
+  };
+
+  const onSubmit1 = async (e) => {
+    e.preventDefault();
+    if (!passwordValid()) {
+      return;
+    }
+    try {
+      const checkResponse = await axios.get(
+        "http://192.168.0.209:8090/signup",
+        {
+          params: { email: Memberemail },
+        }
+      );
+      if (checkResponse.data.exists) {
+        alert("이미 사용 중인 이메일 주소입니다.");
+        return;
+      }
+      const response = await axios.post("http://192.168.0.209:8090/signup", {
+        email: Memberemail,
+        password: Memberpassword,
+        nickname: Username,
+        phone: Userphonenumber,
+      });
+      if (response.status === 200) {
+        goHome();
+      }
+    } catch (error) {
+      if (error.response) {
+        alert(`회원가입 중 오류가 발생했습니다. `);
+      }
+    }
+  };
+
   return (
-    <form className="member">
-      <h2>회원가입</h2>
+    <div className="membership-page">
+      <nav className="NavMenu">
+        <Login />
+        <button onClick={goMypage} className="NavMenuTitle">
+          마이페이지 이동
+        </button>
+        <button onClick={goBoard} className="NavMenuTitle">
+          고객지원
+        </button>
+        <button onClick={goAdmin} className="NavMenuTitle">
+          관리자페이지
+        </button>
+      </nav>
+      <form className="membership_form" onSubmit={onSubmit1}>
+        <h1 onClick={goHome}>P H O P O</h1>
+        <h2>회원가입</h2>
 
-      <div>
-        <input
-          type="email"
-          name="email"
-          placeholder="이메일 입력"
-          required
-          autoComplete="email"
-        />
-      </div>
+        <div>
+          <input
+            type="email"
+            name="email"
+            value={Memberemail}
+            required
+            placeholder="이메일 입력"
+            onChange={onMemberemail}
+          />
+        </div>
 
-      <div>
-        <input type="password" id="password" placeholder="비밀번호 입력" />
-      </div>
+        <div className="password-input">
+          <input
+            type="password"
+            value={Memberpassword}
+            id="password"
+            required
+            pattern=".{8,}"
+            title="최소 8자 이상 입력하세요."
+            placeholder="비밀번호 입력"
+            onChange={onMemberpassword}
+          />
+        </div>
+        <div>
+          <input
+            type="password"
+            value={Memberpassword1}
+            name="password1"
+            required
+            pattern=".{8,}"
+            title="최소 8자 이상 입력하세요."
+            placeholder="비밀번호 확인"
+            onChange={onMemberpassword1}
+          />
+        </div>
 
-      <div>
-        <input type="password" id="password1" placeholder="비밀번호 확인" />
-      </div>
+        <div>
+          <input
+            type="text"
+            value={Username}
+            name="nickname"
+            required
+            placeholder="사용자 이름"
+            onChange={onUsername}
+          />
+        </div>
 
-      <div>
-        <input type="text" placeholder="사용자 이름" />
-      </div>
+        <div>
+          <input
+            type="tel"
+            value={Userphonenumber}
+            name="phone"
+            required
+            placeholder="휴대폰번호"
+            onChange={onUserphonenumber}
+          />
+        </div>
 
-      <div>
-        <input type="tel" id="phone" placeholder="휴대폰번호" />
-      </div>
+        <br />
 
-      <br />
-
-      <button type="submit">회원가입</button>
-    </form>
+        <button type="submit">가입하기</button>
+      </form>
+    </div>
   );
 }
-
-export default MemberShip;
