3f129eb5ac3c37fcb8424b00019a8f1e6ae5ee25
5junoyung <dyrtiq12@naver.com>
2023-03-30 18:53:38 +0900

    관리자페이지수정

--- src/components/Announcement.js
+++ src/components/Announcement.js
@@ -1,28 +1,141 @@
-import React, { useState } from "react";
-import { getAnnouncements } from "../utils/api";
+import React, { useState, useEffect } from "react";
+import {
+  addAnnouncement,
+  getAnnouncements,
+  deleteAnnouncement,
+  editAnnouncement,
+} from "../utils/api";
 
 function Announcement() {
+  const [title, setTitle] = useState("");
   const [announcement, setAnnouncement] = useState("");
+  const [announcements, setAnnouncements] = useState([]);
+  const [showList, setShowList] = useState(false);
+  const [editingAnnouncement, setEditingAnnouncement] = useState(null);
 
-  const handleSubmit = (event) => {
+  const handleSubmit = async (event) => {
     event.preventDefault();
-    getAnnouncements(announcement);
-    setAnnouncement("");
+    try {
+      const token = localStorage.getItem("token");
+      const response = await addAnnouncement(title, announcement, token);
+      console.log(response);
+      setTitle("");
+      setAnnouncement("");
+      alert("등록이 완료되었습니다.");
+    } catch (error) {
+      console.error(error);
+    }
   };
 
+  const handleDeleteAnnouncement = async (announcementId) => {
+    const token = localStorage.getItem("token");
+    try {
+      await deleteAnnouncement(announcementId, token);
+      if (window.confirm("정말 삭제하시겠습니까?")) {
+        window.location.reload();
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handleEditAnnouncement = async (event) => {
+    event.preventDefault();
+    try {
+      const token = localStorage.getItem("token");
+      await editAnnouncement(editingAnnouncement, title, announcement, token);
+      setTitle("");
+      setAnnouncement("");
+      setEditingAnnouncement(null);
+      alert("수정이 완료되었습니다.");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+  const handleToggleList = () => {
+    setShowList(!showList);
+  };
+
+  useEffect(() => {
+    const fetchAnnouncements = async () => {
+      try {
+        const response = await getAnnouncements();
+        setAnnouncements(response);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    fetchAnnouncements();
+  }, []);
+  const handleEditClick = (announcement) => {
+    setEditingAnnouncement(announcement);
+    setTitle(announcement.title);
+    setAnnouncement(announcement.content);
+  };
   return (
     <div className="Announcement">
       <h2>공지사항</h2>
-      <form onSubmit={handleSubmit}>
+      <form
+        onSubmit={editingAnnouncement ? handleEditAnnouncement : handleSubmit}
+      >
         <label>
-          공지사항 내용:
+          제목:
+          <input
+            type="text"
+            value={title}
+            onChange={(event) => setTitle(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          내용:
           <textarea
             value={announcement}
             onChange={(event) => setAnnouncement(event.target.value)}
           />
         </label>
-        <button type="submit">등록</button>
+        <button type="submit">
+          {editingAnnouncement ? "수정하기" : "등록"}
+        </button>
+        {editingAnnouncement && (
+          <button type="button" onClick={() => setEditingAnnouncement(null)}>
+            취소
+          </button>
+        )}
       </form>
+      <h3>
+        공지사항 목록{" "}
+        <button onClick={handleToggleList}>
+          {showList ? "목록 숨기기" : "목록 보이기"}
+        </button>
+      </h3>
+      {showList && ( // showList가 true일 때만 목록을 표시하도록 함
+        <>
+          <ul>
+            {announcements.map((announcement) => (
+              <div key={announcement.id} className="announcement-item">
+                <h4>{announcement.title}</h4>
+                <button onClick={() => handleDeleteAnnouncement(announcement)}>
+                  삭제
+                </button>
+                <button onClick={() => handleEditClick(announcement)}>
+                  수정
+                </button>
+                <p className="announcement_content">{announcement.content}</p>
+                <div className="announcement_at">
+                  <span>
+                    생성일: {new Date(announcement.created_at).toLocaleString()}
+                  </span>
+                  &nbsp;&nbsp;&nbsp;
+                  <span>
+                    수정일:
+                    {new Date(announcement.modified_at).toLocaleString()}
+                  </span>
+                </div>
+              </div>
+            ))}
+          </ul>
+        </>
+      )}
     </div>
   );
 }
--- src/utils/api.js
+++ src/utils/api.js
@@ -1,22 +1,73 @@
-// 백엔드 서버 주소
-const SERVER_URL = "http://localhost:8000";
+import axios from "axios";
 
-// 계정 삭제 API 요청을 보내는 함수
-export const deleteAccount = async (username) => {
-  const response = await fetch(`${SERVER_URL}/api/accounts/${username}`, {
-    method: "DELETE",
-  });
-  if (!response.ok) {
-    throw new Error("계정을 삭제 실패");
+export const addAnnouncement = async (title, content, token) => {
+  try {
+    const response = await axios.post(
+      "http://192.168.0.209:8090/notice/add",
+      { title, content },
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return null;
   }
 };
 
-// 공지사항 API 요청을 보내는 함수
 export const getAnnouncements = async () => {
-  const response = await fetch(`${SERVER_URL}/api/announcements`);
-  if (!response.ok) {
-    throw new Error("공지사항 업로드 실패");
+  try {
+    const token = localStorage.getItem("token");
+    const response = await axios.get("http://192.168.0.209:8090/notice", {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return [];
   }
-  const data = await response.json();
-  return data;
+};
+export const deleteAnnouncement = async (announcementId, token) => {
+  try {
+    const response = await axios.delete(
+      `http://192.168.0.209:8090/notice/${announcementId.notice_id}`,
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    return response.data;
+  } catch (error) {
+    throw new Error(error.response.data);
+  }
+};
+export const editAnnouncement = async (
+  editingAnnouncement,
+  title,
+  content,
+  token
+) => {
+  try {
+    await axios.put(
+      `http://192.168.0.209:8090/notice/${editingAnnouncement.notice_id}`,
+      { title, content }, // title과 content 정보를 모두 전달
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    );
+    // 이전에 수정한 것처럼, `editingAnnouncement` 객체를 직접 수정하면 더 좋을 것입니다.
+    // 수정된 필드 값을 서버에서 반환받은 필드 값으로 덮어쓰는 방법을 사용하셔도 됩니다.
+    editingAnnouncement.title = title;
+    editingAnnouncement.content = content;
+  } catch (error) {
+    throw new Error(error.response.data);
+  }
 };
